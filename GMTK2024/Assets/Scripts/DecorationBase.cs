1b3e71346525cc536b48d06367e4dcaabcbf2847
malachek <malachykennedy1@gmail.com>
2024-08-17 18:18:39 -0700

    start on switch delegates

--- GMTK2024/Assets/Scripts/DecorationBase.cs
+++ GMTK2024/Assets/Scripts/DecorationBase.cs
@@ -1,4 +1,5 @@
 ﻿using Cinemachine;
+using System.Linq.Expressions;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -11,33 +12,76 @@ public class DecorationBase : MonoBehaviour
 
     CinemachineBrain brain;
 
+    [SerializeField]
+    Transform godLook;
+    //rotation?????
+  
+    [SerializeField]
+    Transform playerLocation;
+
+    protected bool isGodMode;
+
+    delegate void OnOrientationUpdate();
+    OnOrientationUpdate onOrientationUpdate;
 
 
     protected virtual void Awake()
     {
-        PlayerSwitch.OnSwitchPlayer += SwitchLookLocation;
+        //PlayerSwitch.OnSwitchPlayer += HandleSwitchPlayer;
+
+        PlayerSwitch.OnSwitchToGod += HandleSwitchToGod;
+        PlayerSwitch.OnSwitchToSmall += HandleSwitchToSmall;
 
         brain = Camera.main.GetComponent<CinemachineBrain>();
 
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = decorData.Sprite;
+    }
+
+    protected virtual void Start()
+    {
 
     }
 
+    protected void HandleSwitchToGod()
+    {
+        isGodMode = true;
+    }
+
+    protected void HandleSwitchToSmall()
+    {
+        isGodMode = false;
+    }
+
+    public void HandleSwitchPlayer(string PlayerMode)
+    {
+        SwitchLookLocation(PlayerMode.Equals("GOD"));
+    }
+
+
+
     protected virtual void Update()
     {
-        if (brain == null) return;
+        LookUpdate;
+
+        transform.LookAt(CurrentLook);
+
+        /* if (brain == null) return;
         
         CinemachineVirtualCamera activeVirtualCamera = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
 
         Vector3 cameraPosition = activeVirtualCamera.transform.position;
 
-        transform.LookAt(Vector3.Scale(cameraPosition, new Vector3(1f, 0f, 1f)), Vector3.up);
+        transform.LookAt(Vector3.Scale(cameraPosition, new Vector3(1f, 0f, 1f)), Vector3.up);*/
     }
 
-    public void SwitchLookLocation()
+    public void SwitchLookLocation(bool isGod)
     {
         Debug.Log($"Plant: {name} is switching look location now");
-        brain = Camera.main.GetComponent<CinemachineBrain>();
+        smallLook = isGod ? godLook : smallLook;
+
+
+       
+        /*brain = Camera.main.GetComponent<CinemachineBrain>();*/
     }
 }
--- GMTK2024/Assets/Scripts/Player/PlayerSwitch.cs
+++ GMTK2024/Assets/Scripts/Player/PlayerSwitch.cs
@@ -12,14 +12,27 @@ public class PlayerSwitch : MonoBehaviour
     [SerializeField] bool startInGod;
     public bool godIsActive {  get; private set; }
 
-    public delegate void SwitchPlayerAction();
-    public static event SwitchPlayerAction OnSwitchPlayer;
+    //public delegate void SwitchPlayerAction(string PlayerMode);
+    //public static event SwitchPlayerAction OnSwitchPlayer;
+
+    public delegate void SwitchToGod();
+    public static event SwitchToGod OnSwitchToGod;
+
+    public delegate void SwitchToSmall();
+    public static event SwitchToSmall OnSwitchToSmall;
+
+
+    [Header("Cameras")]
+
+    public Transform CurrentGodCameraTransform;
+    private Transform OriginalGodCameraTransform;
 
     private void Awake()
     {
         godIsActive = startInGod;
         SetEnables();
         Debug.Log(godIsActive ? "Starting in GOD MODE" : "Starting in SMALL MODE");
+        OriginalGodCameraTransform = CurrentGodCameraTransform;
     }
 
     void Update()
@@ -30,15 +43,43 @@ public class PlayerSwitch : MonoBehaviour
         }
     }
 
+    /*public static void TriggerSwitchPlayer(string PlayerMode)
+    {
+        OnSwitchPlayer?.Invoke(PlayerMode); //Either "GOD" or "SMALL"
+    }*/
+
+    
+
+    //Calls SwitchPlayer event, flipflops between GOD and SMALL
     public void SwitchPlayer()
     {
-        if (OnSwitchPlayer != null)
-        {
-            OnSwitchPlayer();
-        }
-        Debug.Log(godIsActive ? "switching from god to small" : "switching from small to god");
+        Debug.Log(godIsActive ? "switching from GOD to SMALL" : "switching from SMALL to GOD");
         godIsActive = !godIsActive;
+
+        if (godIsActive)
+        {
+            TriggerSwitchToGod();
+        }
+        else
+        {
+            TriggerSwitchToSmall();
+        }
+
+        /*if (OnSwitchPlayer != null)
+        {
+            //OnSwitchPlayer(godIsActive ? "GOD" : "SMALL");
+            TriggerSwitchPlayer(godIsActive ? "GOD" : "SMALL");
+        }*/
+
         SetEnables();
+    }
+    public static void TriggerSwitchToGod()
+    {
+        OnSwitchToGod?.Invoke();
+    }
+    public static void TriggerSwitchToSmall()
+    {
+        OnSwitchToSmall?.Invoke();
     }
 
     private void SetEnables()
