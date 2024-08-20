ec44dc12116de665510cb9071aded2e2159c124d
malachek <malachykennedy1@gmail.com>
2024-08-20 03:24:27 -0700

    interior design

---
+++ GMTK2024/Assets/Scripts/UI/TutorialManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //OnGameStart += Welcome
+
+    }
+
+    void Welcome()
+    {
+
+    }
+
+    void Planting()
+    {
+
+    }
+
+    void Growing()
+    {
+
+    }
+
+    void Shrinking()
+    {
+
+    }
+
+    void Shop()
+    {
+
+    }
+
+    void FullyGrown()
+    {
+
+    }
+
+    void GameEnd()
+    {
+
+    }
+}
